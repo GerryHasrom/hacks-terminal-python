771bece456d4c805048e0ff009c50835827b6cca
Gerry Hasrom <gerryhasrom25@gmail.com>
2025-09-14 11:38:00 +0800

    nambah hacks 2

---
+++ hack_02_color_text.py
@@ -0,0 +1,15 @@
+print("cek dokumentasi resmi https://en.wikipedia.org/wiki/ANSI_escape_code#Colors")
+print("===========================================================================")
+# Foreground colors
+print("\033[31mhacks color terminal python\033[0m")
+print("\033[32mhacks color terminal python\033[0m")
+print("\033[33mhacks color terminal python\033[0m")
+print("\033[34mhacks color terminal python\033[0m")
+print("\033[35mhacks color terminal python\033[0m")
+print("\033[36mhacks color terminal python\033[0m")
+print("\033[37mhacks color terminal python\033[0m")
+
+# Background colors
+print("\033[41mhacks color terminal python\033[0m")
+print("\033[42mhacks color terminal python\033[0m")
+print("\033[44mhacks color terminal python\033[0m")
